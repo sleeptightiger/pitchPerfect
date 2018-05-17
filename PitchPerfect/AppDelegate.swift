bb2086a9e9ecd31f65abf3bbd5d6d4c474022106
Gerry Morales Meza <sleep.tiger2@gmail.com>
2018-05-17 04:21:15 -0500

    first commit

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -12,10 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("Hello, World!")
         return true
     }
 
